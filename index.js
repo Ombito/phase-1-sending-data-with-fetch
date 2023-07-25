611bb529e9a6257a025b27ccb318aa031bc10c2e
ombito <alvinombito09@gmail.com>
2023-07-25 20:24:42 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+
+const submitData = () => {
+
+    const userData = {
+        name: "Steve",
+        email: "steve@steve.com",
+    }
+    fetch("http://localhost:3000/users", {
+    method: 'POST',
+    headers:{
+        'Content-Type': 'application/json',
+        Accept: "application/json"
+    },   
+    body:JSON.stringify(userData)
+})
+
+.then(response => response.json())
+.then(data => {
+    let newData = document.createElement('p')
+    newData.textContent = data.id
+    body.appendChild(newData)
+
+
+     console.log(data)
+.catch(error => {
+    let newParagraph = document.createElement('p')
+    let errorMessage = 'Unauthorized Access'
+
+    newParagraph.textContent = errorMessage
+    body.appendChild(newParagraph)
+    console.log(error)})
+})
+}
+submitData()
